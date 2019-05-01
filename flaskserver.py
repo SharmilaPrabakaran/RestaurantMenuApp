d8a426d4986835971bcee06d49860ec6b93421df
Sharmila Prabakaran <38150422+SharmilaPrabakaran@users.noreply.github.com>
2019-05-01 09:44:59 -0700

    Adding JSON

--- flaskserver.py
+++ flaskserver.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
 
 
@@ -11,12 +12,30 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+    Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(Menu_Item=Menu_Item.serialize)
+
+
+@app.route('/restaurant/JSON')
+def restaurantsJSON():
+    restaurants = session.query(Restaurant).all()
+    return jsonify(restaurants=[r.serialize for r in restaurants])
+
 @app.route('/')
 @app.route('/restaurant/')
 def showRestaurants():
   restaurant = session.query(Restaurant).all()
   return render_template('restaurants.html', restaurant=restaurant)
-  return "Show all restaurants"
 
 @app.route('/restaurant/new/', methods = ['GET','POST'])
 def newRestaurant():
